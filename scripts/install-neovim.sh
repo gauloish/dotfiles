a2b5ede38ce912ff591aeab87e96336b15138df4
gauloish <125079293+gauloish@users.noreply.github.com>
2025-12-10 00:48:08 -0300

    feat: add neovim installer

---
+++ scripts/install-neovim.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+dnf_packages=(
+    # Python
+    python
+    pip
+
+    # C/C++:
+    gcc-c++
+
+    # Java:
+    java
+    java-devel
+    java-latest-openjdk-devel
+
+    # Javascript:
+    nodejs
+    npm
+
+    # Rust:
+    rust
+    cargo
+
+    # Lua:
+    lua
+    luarocks
+
+    # Latex
+    texlive-scheme-basic
+
+    # Neovim:
+    neovim
+    python3-neovim
+)
+
+pip_packages=(
+    pynvim
+    yarp
+    jupyter
+    jupyterlab
+    jupytext
+)
+
+npm_packages=(
+    neovim
+)
+
+sudo dnf install -y "${dnf_packages[@]}"
+pip install -y "${pip_packages[@]}"
+sudo npm install -g "${npm_packages[@]}"
+
+echo 'export JAVA_HOME=$(readlink -f /usr/bin/java | sed "s:/bin/java::")' >> ~/.bashrc
