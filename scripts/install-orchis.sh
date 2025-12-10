ddc4064197bda9d3eb01335929c571608e9a80da
gauloish <125079293+gauloish@users.noreply.github.com>
2025-12-10 00:48:27 -0300

    feat: add orchis installer

---
+++ scripts/install-orchis.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+dnf_packages=(
+    # Gnome:
+    gnome-themes-extra
+	gnome-tweaks
+	gnome-extensions-app
+	gnome-shell-extension-user-theme
+	gnome-shell-extension-blur-my-shell
+
+    # GTK:
+	gtk-murrine-engine
+
+    # Others:
+	sassc
+)
+
+sudo dnf install -y "${dnf_packages[@]}"
+
+rm -rf ~/tmp/Orchis-theme
+git -C ~/tmp clone https://github.com/vinceliuice/Orchis-theme.git
+sh ~/tmp/Orchis-theme/install.sh --uninstall
+sh ~/tmp/Orchis-theme/install.sh --theme grey --color dark --size standard --tweaks solid compact submenu dock --libadwaita --fixed
