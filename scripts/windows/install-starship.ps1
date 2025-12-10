eec3a4817455d7d11dc4dc4ef72a328549bb74c0
gauloish <125079293+gauloish@users.noreply.github.com>
2025-12-10 19:13:51 -0300

    feat: add starship prompt in powershell

--- scripts/windows/install-starship.ps1
+++ scripts/windows/install-starship.ps1
@@ -1,1 +1,13 @@
 scoop install starship
+
+$starshipCode = "Invoke-Expression (&starship init powershell)"
+
+if (-not (Test-Path $PROFILE)) {
+    New-Item -Path $PROFILE -ItemType File -Force | Out-Null
+} 
+
+$profileContent = Get-Content $PROFILE -Raw
+
+if ($profileContent -notlike "*$starshipCode*") {
+    Add-Content -Path $PROFILE -Value $starshipCode
+} 
